556385a6b6180fae3df1f5a92d9de5defabe3c59
Sanamain1229 <melvicpasia1229@gmail.com>
2025-09-24 08:04:19 +0800

    Sequencing in words

---
+++ Is Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        if len(s) <= 0:
+            return True
+        
+        s = list(s)
+        for i in t:
+            if s[0] == i:
+                s.pop(0)
+    
+            if len(s) == 0:
+                return True
+        
+        return False
+
+solution = Solution()
+print (solution.isSubsequence("", "ahbgdc"))

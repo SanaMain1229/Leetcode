0a285f39819f7d0e9fed85e114c65488b2b6a89c
Sanamain1229 <melvicpasia1229@gmail.com>
2025-09-24 17:10:05 +0800

    Finding the pivot index

---
+++ Find Pivot Index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        curr_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            if left_sum == curr_sum - left_sum - nums[i]:
+                return i
+            left_sum += nums[i]
+
+        return -1 
+solution = Solution()
+print (solution.pivotIndex([1,7,3,6,5,6]))
